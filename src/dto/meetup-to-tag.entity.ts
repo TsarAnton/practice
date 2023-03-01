aef88f5a0cc34b7723d2fc53751f13a73b674891
TsarekAntoha <tsaranton2006@gmail.com>
2023-03-01 08:56:36 +0300

    added dto for meetup, tag, meetup-to-tag

---
+++ src/dto/meetup-to-tag.entity.ts
@@ -0,0 +1,4 @@
+export class CreateMeetupToTagDto {
+    meetupId: number;
+    tagId: number;
+}
---
+++ src/dto/meetup.dto.ts
@@ -0,0 +1,13 @@
+export class CreateMeetupDto {
+    name: string;
+    description: string;
+    time: Date;
+    place: string;
+}
+
+export class UpdateMeetupDto {
+    name: string;
+    description: string;
+    time: Date;
+    place: string;
+}
