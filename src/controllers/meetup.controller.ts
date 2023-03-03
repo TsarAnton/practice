0a6e879482a815de02579aeed8734a705baeb5d7
TsarekAntoha <tsaranton2006@gmail.com>
2023-03-03 08:40:39 +0300

    added controller for meetup

---
+++ src/controllers/meetup.controller.ts
@@ -0,0 +1,52 @@
+import { Controller, Get, Post, Delete, Param, Body, Put, NotFoundException } from '@nestjs/common';
+import { Meetup } from '../entities/meetup.entity';
+import { MeetupService } from '../services/meetup.service';
+import { CreateMeetupDto, UpdateMeetupDto } from '../dto/meetup.dto';
+
+@Controller('meetups')
+export class MeetupController {
+  constructor(private readonly MeetupService: MeetupService){
+  }
+
+  @Get()
+  getAllAction(): Promise<Meetup[]> {
+    return this.MeetupService.readAll();
+  }
+
+  @Get(':id')
+  async getOneAction(@Param('id') id: number): Promise<Meetup> {
+    const meetup = await this.MeetupService.readById(id);
+    if( meetup === null ){
+      throw new NotFoundException(`Meetup with id=${id} does not exist`);
+    }
+    return meetup;
+  }
+
+  @Post()
+  async createAction(@Body() meetup: CreateMeetupDto): Promise<Meetup>{
+    const newMeetup = new Meetup();
+    newMeetup.name = meetup.name;
+    newMeetup.description = meetup.description;
+    newMeetup.place = meetup.place;
+    newMeetup.time = meetup.time;
+    return this.MeetupService.create(newMeetup);
+  }
+
+  @Put(':id')
+  async updateAction(@Param('id') id: number, @Body() meetup: UpdateMeetupDto): Promise<Meetup> {
+      const existingMeetup = await this.MeetupService.readById(id);
+      if(existingMeetup === null){
+        throw new NotFoundException(`Meetup with id=${id} does not exist`);
+      }
+      existingMeetup.name = meetup.name;
+      existingMeetup.description = meetup.description;
+      existingMeetup.place = meetup.place;
+      existingMeetup.time = meetup.time;
+      return this.MeetupService.update(existingMeetup);
+  }
+
+  @Delete(':id')
+  deleteAction(@Param('id') id: number): Promise<void>{
+    return this.MeetupService.delete(id);
+  }
+}
