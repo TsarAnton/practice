83b3adeee9902cf085b89f0d93da0964b63a6dde
TsarekAntoha <tsaranton2006@gmail.com>
2023-06-01 12:37:11 +0300

    hollow

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm'
 import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [CoreModule, TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '2003',
-    database: 'practice_db',
-    entities: ["./core/entities/*.entity{.ts,.js}"],
-    autoLoadEntities: true,
-    synchronize: true,
-  })],
+  imports: [CoreModule],
   controllers: [],
   providers: [],
 })
--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,40 +1,17 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { Meetup } from './entities/meetup.entity';
-import { Tag } from './entities/tag.entity';
-import { MeetupToTag } from './entities/meetup-to-tag.entity';
-
-import { MeetupService } from './services/meetup.service';
-import { TagService } from './services/tag.service';
-import { MeetupToTagService } from './services/meetup-to-tag.service';
-
-import { MeetupController } from './controllers/meetup.controller';
-import { TagController } from './controllers/tag.controller';
-import { MeetupToTagController } from './controllers/meetup-to-tag.controller';
 
 @Module({
 	imports: [
-		TypeOrmModule.forFeature([
-			Meetup,
-    	    Tag,
-        	MeetupToTag,
-		]),
+		
 	],
 	controllers: [
-		MeetupController,
-		MeetupToTagController,
-		TagController,
+		
 	],
 	providers: [
-		MeetupService,
-    	TagService,
-	    MeetupToTagService,
+		
 	],
 	exports: [
-		MeetupService,
-    	TagService,
-	    MeetupToTagService,
+		
 	],
 })
 export class CoreModule {}
